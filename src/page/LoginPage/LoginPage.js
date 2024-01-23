cbb5fd283009ad539af3419b22cf0474f3ac995c
Minhtuan25022002 <ntmt52502@gmail.com>
2024-01-23 17:41:47 +0700

    validate login

--- src/page/LoginPage/LoginPage.js
+++ src/page/LoginPage/LoginPage.js
@@ -1,83 +1,94 @@
-import React, { useEffect, useState } from "react";
-import './LoginPage.scss'
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { handleLoginAdmin } from "../../services/userService";
-import _ from "lodash";
 import { useNavigate } from "react-router-dom";
+import './LoginPage.scss';
+import _ from "lodash";
 
-function LoginPage() {
-    const [userName, setUserName] = useState('')
-    const [passWord, setPassWord] = useState('')
-    const [showPassWord, SetShowPasswords] = useState(false);
+const LoginPage = () => {
     const navigate = useNavigate();
 
-    const handleShowHidePassword = () => {
-        SetShowPasswords(!showPassWord);
-    }
-
-    const handleOnChangeUsername = (e) => {
-        setUserName(e.target.value)
-    }
-
-    const handleOnChangePassword = (e) => {
-        setPassWord(e.target.value)
-    }
-
-    const handleLogin = async () => {
-        try {
-            let data = await handleLoginAdmin(userName, passWord)
-            if(data && !_.isEmpty(data.token)) {
-                localStorage.setItem('token', data.token)
-                localStorage.setItem('userInfo', data.user.username)
-                window.location.href = '/admin';
+    const formik = useFormik({
+        initialValues: {
+            userName: '',
+            passWord: '',
+        },
+        validationSchema: Yup.object({
+            userName: Yup.string().required('Username is required'),
+            passWord: Yup.string().required('Password is required'),
+        }),
+        onSubmit: async (values, { setSubmitting, setErrors }) => {
+            try {
+                let data = await handleLoginAdmin(values.userName, values.passWord);
+                if (data && !_.isEmpty(data.token)) {
+                    localStorage.setItem('token', data.token);
+                    localStorage.setItem('userInfo', data.user.username);
+                    navigate('/admin');
+                }
+            } catch (error) {
+                console.error(error);
+                setErrors({ general: 'An error occurred during login.' });
+            } finally {
+                setSubmitting(false);
             }
-        } catch (error) {
-            //chưa xử lí validate
-            console.log(error);
-        }
-    }
+        },
+    });
 
-    return ( 
+    return (
         <div className='login-background'>
-                <div className='login-container'>
-                    <div className='login-content row'>
-                        <div className='col-12 text-login'>Login</div>
-                        <div className='col-12 form-group login-input'>
+            <div className='login-container'>
+                <div className='login-content row'>
+                    <div className='col-12 text-login'>Login</div>
+                    <form onSubmit={formik.handleSubmit} className='col-12'>
+                        <div className='form-group login-input'>
                             <label>Username:</label>
-                            <input 
-                                type='text' 
-                                className='form-control' 
+                            <input
+                                type='text'
+                                className='form-control'
                                 placeholder='Enter your username'
-                                value={userName}
-                                onChange={handleOnChangeUsername}
+                                name='userName'
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.userName}
                             />
+                            {formik.touched.userName && formik.errors.userName && (
+                                <div className='error-message'>{formik.errors.userName}</div>
+                            )}
                         </div>
-                        <div className='col-12 form-group login-input'>
+                        <div className='form-group login-input'>
                             <label>Password:</label>
                             <div className='custom-input-password'>
-                                <input 
-                                    type={showPassWord ? 'text' : 'password'}
-                                    className='form-control' 
+                                <input
+                                    type={formik.values.showPassword ? 'text' : 'password'}
+                                    className='form-control'
                                     placeholder='Enter your password'
-                                    value={passWord}
-                                    onChange={handleOnChangePassword}
+                                    name='passWord'
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    value={formik.values.passWord}
                                 />
-                                <span onClick={() => handleShowHidePassword()}>
-                                    {
-                                        showPassWord ? <i className="fa fa-eye" aria-hidden="true"></i> : <i className="fa fa-eye-slash" aria-hidden="true"></i> 
-                                    }
+                                <span onClick={() => formik.setFieldValue('showPassword', !formik.values.showPassword)}>
+                                    {formik.values.showPassword ? <i className="fa fa-eye" aria-hidden="true"></i> : <i className="fa fa-eye-slash" aria-hidden="true"></i>}
                                 </span>
                             </div>
+                            {formik.touched.passWord && formik.errors.passWord && (
+                                <div className='error-message'>{formik.errors.passWord}</div>
+                            )}
                         </div>
-                        <div className='col-12' style={{ color: 'red' }}>
-                            {/* {this.state.errMessage} */}
+                        <div className='error-message col-12'>
+                            {formik.errors.general}
                         </div>
                         <div className='col-12'>
-                            <button className='btn-login' onClick={handleLogin}>Login</button>
+                            <button type='submit' className='btn-login' disabled={formik.isSubmitting}>
+                                Login
+                            </button>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
+        </div>
     );
-}
+};
 
 export default LoginPage;
