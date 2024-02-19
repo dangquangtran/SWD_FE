ceb6090a8ec19f4fc9bb37ff434b4f5885ddae42
Truong Thuy Truc <truc5302@gmail.com>
2024-02-19 23:21:51 +0700

    Create main club UI

--- src/page/NewFeed/NewFeed.js
+++ src/page/NewFeed/NewFeed.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import "./NewFeed.scss"
+import "./NewFeed.scss";
+import image1 from "../../assets/Sport/badminton.jpg";
+
 
 function NewFeed() {
 
     return (
         <div className="new-feed-container">
-            <div className="club-title-new-feed">CLB xxx</div>
+            <div className="club-title-new-feed">CLB Cầu Lông</div>
             <div className="post-container">
                 <input className="post-input" placeholder="Tìm kiếm người chơi" />
                 <div className="post-line"></div>
@@ -15,8 +17,21 @@ function NewFeed() {
             <h5>Bài viết mới nhất</h5>
 
             <div className="main-post-container">
-                <div className="poster-name"><p>Truc</p></div>
-                <div className="caption">mình cần tìm.....</div>
+                <div className="poster-name"><p>Truc</p><div>posted time</div></div>
+                <div className="caption">Mình cần tìm.....</div>
+                <div className="post-content-container">
+                    <img className="post-img" src={image1} />
+                    <div className="post-infor">
+                        <h3>Thông tin trận đấu</h3>
+                        <div>
+                            <div><b>Sân:</b></div>
+                            <div><b>Thời gian:</b></div>
+                            <div><b>Số lượng:</b></div>
+                            <div><b>Còn: </b></div>
+                        </div>
+                        <button className="btn-join">Join</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
